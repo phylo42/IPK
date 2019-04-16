00da6ea5512308b57bbc209d43b1ec74bd8bcbab
Nikolai Romashchenko <nikolai.romashchenko@lirmm.fr>
2019-04-16 13:20:34 +0200

    Refactoring CMake structure

--- include/core/version.h
+++ include/core/version.h
@@ -5,17 +5,17 @@ namespace core
 {
     /// \brief Core version class. Follows the semantic versioning 2.0 rules.
     /// \sa Semantic versioning 2.0: https://semver.org/
+    /// TODO: fill this automatically with the CMakeLists.txt definitions
     struct version
     {
         static constexpr auto major = "0";
         static constexpr auto minor = "1";
         static constexpr auto revision = "0";
-        static constexpr auto suffix = "alpha";
 
         /// \brief Returns the core version number as a string
         static constexpr std::string as_string() const
         {
-            return major + "." + minor + "." + revision + (suffix ? "-" + suffix : "");
+            return major + "." + minor + "." + revision;
         }
     };
 }
