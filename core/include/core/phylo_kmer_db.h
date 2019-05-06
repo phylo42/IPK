0a3c0f7343c395ce11da7caead7196b75e406481
Nikolai Romashchenko <nikolai.romashchenko@lirmm.fr>
2019-05-06 15:11:28 +0200

    Conditional compilation

--- core/include/core/phylo_kmer_db.h
+++ core/include/core/phylo_kmer_db.h
@@ -1,14 +1,24 @@
 #ifndef RAPPAS_CORE_PHYLO_KMER_DB_H
 #define RAPPAS_CORE_PHYLO_KMER_DB_H
 
+
+#ifdef USE_SKA_FLAT_HASH_MAP
 #include <flat_hash_map/flat_hash_map.hpp>
+#elif USE_SKA_BYTELL_HASH_MAP
 #include <flat_hash_map/bytell_hash_map.hpp>
+#elif USE_ABSL_FLAT_HASH_MAP
 #include <absl/container/flat_hash_map.h>
+#elif USE_FOLLY_F14_FAST_MAP
 #include <folly/container/F14Map.h>
+#elif USE_PHMAP_FLAT_HASH_MAP
 #include <parallel_hashmap/phmap.h>
+#elif USE_TSL_ROBIN_MAP
 #include <tsl/robin_map.h>
+#elif USE_TSL_HOPSCOTCH_MAP
 #include <tsl/hopscotch_map.h>
-#include <robin_hood.h>
+#endif
+
+//#include <robin_hood.h>
 
 #include "phylo_kmer.h"
 
@@ -29,14 +39,21 @@ namespace boost
 namespace core
 {
     template<typename... Args>
+#ifdef USE_SKA_FLAT_HASH_MAP
     using hash_map = ska::flat_hash_map<Args...>;
-    //using hash_map = ska::bytell_hash_map<Args...>;
-    //using hash_map = absl::flat_hash_map<Args...>;
-    //using hash_map = folly::F14FastMap<Args...>;
-    //using hash_map = phmap::flat_hash_map<Args...>;
-    //using hash_map = tsl::robin_map<Args...>;
-    //using hash_map = tsl::hopscotch_map<Args...>;
-
+#elif USE_SKA_BYTELL_HASH_MAP
+    using hash_map = ska::bytell_hash_map<Args...>;
+#elif USE_ABSL_FLAT_HASH_MAP
+    using hash_map = absl::flat_hash_map<Args...>;
+#elif USE_FOLLY_F14_FAST_MAP
+    using hash_map = folly::F14FastMap<Args...>;
+#elif USE_PHMAP_FLAT_HASH_MAP
+    using hash_map = phmap::flat_hash_map<Args...>;
+#elif USE_TSL_ROBIN_MAP
+    using hash_map = tsl::robin_map<Args...>;
+#elif USE_TSL_HOPSCOTCH_MAP
+    using hash_map = tsl::hopscotch_map<Args...>;
+#endif
     /// requires manual intervention (does not support noexcept destructor and can not be defined this way)
     ///using hash_map = robin_hood::unordered_flat_map<Args...>;
     ///using hash_map = robin_hood::unordered_map<Args...>;
