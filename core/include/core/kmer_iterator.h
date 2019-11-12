3f863a57fd54eeb8ca7ec70bf15ac68f11321d0c
Nikolai Romashchenko <nikolai.romashchenko@lirmm.fr>
2019-11-12 16:56:05 +0100

    Restoring lost work on ambiguities

--- core/include/core/kmer_iterator.h
+++ core/include/core/kmer_iterator.h
@@ -2,21 +2,39 @@
 #define RAPPAS_CORE_KMER_ITERATOR_H
 
 #include <string_view>
+#include <vector>
+#include <tuple>
 #include "phylo_kmer.h"
 
 namespace core
 {
+    struct no_ambiguity_policy
+    {
+        /// a k-mer code
+        using value_type = phylo_kmer::key_type;
+    };
+
+    struct one_ambiguity_policy
+    {
+        /// a vector of k-mer codes
+        using value_type = std::vector<phylo_kmer::key_type>;
+    };
+
     /// \brief An iterator class for k-mers of an input sequence.
     /// \details Iterates over all the k-mers of a given sequence and encodes them
     /// in a rolling fashion. Returns a lightweight std::string_view to a current k-mer
     /// of a given sequence and its code.
+    template<typename AmbiguityPolicy>
     class kmer_iterator
     {
     public:
         using iterator_category = std::forward_iterator_tag;
 
-        /// Value of a kmer_iterator, which is a pair of a string view to a k-mer and its code
-        using value_type = std::pair<std::string_view, phylo_kmer::key_type>;
+        /// Return value of a kmer_iterator, which is a pair of a string view to a k-mer
+        /// and a k-mer code storage
+        using value_type = std::pair<std::string_view, typename AmbiguityPolicy::value_type>;
+
+        using pos_t = size_t;
 
         kmer_iterator(std::string_view sequence_view, size_t kmer_size) noexcept;
         kmer_iterator(const kmer_iterator&) = delete;
@@ -25,21 +43,46 @@ namespace core
         kmer_iterator& operator=(kmer_iterator&&) = delete;
         ~kmer_iterator() noexcept = default;
 
-        kmer_iterator& operator++();
+        kmer_iterator& operator++()
+        {
+            bool last_kmer_is_valid = true;
+            bool stop = false;
+            while (!stop)
+            {
+                /// if the last k-mer is valid, we can reuse its code in a rolling fashion
+                /// otherwise we need to calculate the code from scratch
+                const auto encode_result = last_kmer_is_valid ? encode_kmer_from_previous() : encode_kmer();
+                std::tie(_kmer_value, _kmer_position, last_kmer_is_valid) = encode_result;
+                stop = last_kmer_is_valid || (_kmer_position == std::string_view::npos);
+            }
+            return *this;
+        }
 
-        bool operator==(const kmer_iterator& rhs) const noexcept;
-        bool operator!=(const kmer_iterator& rhs) const noexcept;
+        bool operator==(const kmer_iterator& rhs) const noexcept
+        {
+            return (_kmer_position == rhs._kmer_position) && (_sequence_view == rhs._sequence_view);
+        }
+
+        bool operator!=(const kmer_iterator& rhs) const noexcept
+        {
+            return !(*this == rhs);
+        }
 
         value_type operator*() const noexcept;
     private:
-        std::pair<std::pair<phylo_kmer::key_type, size_t>, bool> encode_from(size_t position) const;
-        std::pair<std::pair<phylo_kmer::key_type, size_t>, bool> next_kmer() const;
+        std::tuple<typename AmbiguityPolicy::value_type, pos_t, bool> encode_kmer();
+        std::tuple<typename AmbiguityPolicy::value_type, pos_t, bool> encode_kmer_from_previous() const;
 
         /// String view of the original sequence
         std::string_view _sequence_view;
-        size_t _kmer_position;
         size_t _kmer_size;
-        phylo_kmer::key_type _current_key;
+
+        /// A current stored value of iterator, which type depends on the policy
+        /// (Ex. one or more k-mer codes)
+        typename AmbiguityPolicy::value_type _kmer_value;
+
+        /// A current position of the iterator in the input sequence
+        pos_t _kmer_position;
     };
 
     /// \brief A little proxy class that just creates a k-mer iterator over a sequence.
@@ -47,7 +90,7 @@ namespace core
     class to_kmers
     {
     public:
-        using const_iterator = kmer_iterator;
+        using const_iterator = kmer_iterator<no_ambiguity_policy>;
 
         to_kmers(std::string_view sequence_view, size_t kmer_size) noexcept;
 
--- core/src/core/kmer_iterator.cpp
+++ core/src/core/kmer_iterator.cpp
@@ -4,10 +4,47 @@
 
 using namespace core;
 
-kmer_iterator::kmer_iterator(std::string_view sequence_view, size_t kmer_size) noexcept
-    : _sequence_view{ sequence_view }
-    , _kmer_position{ 0 }
-    , _kmer_size{ kmer_size }
+
+template<>
+std::tuple<no_ambiguity_policy::value_type,
+    kmer_iterator<no_ambiguity_policy>::pos_t,
+    bool> kmer_iterator<no_ambiguity_policy>::encode_kmer()
+{
+    phylo_kmer::key_type key = phylo_kmer::nan_key;
+    bool success = false;
+
+    /// string is too small to start from _kmer_position
+    if (_kmer_position > _sequence_view.size() - _kmer_size)
+    {
+        _kmer_position = std::string_view::npos;
+    }
+    else
+    {
+        const auto kmer_view = _sequence_view.substr(_kmer_position, _kmer_size);
+
+        /// correct k-mer with gaps
+        if (auto code = core::encode_kmer(kmer_view); code)
+        {
+            key = *code;
+            success = true;
+        }
+            /// skip a k-mer if we found a gap or ambiguity
+        else
+        {
+            /// WARNING:
+            /// this is not effective, because we can skip all the bases up to the one
+            /// that is not valid, not just only one. But this is not clear how to return
+            /// this position from core::encode_kmer, still having the API of the core clear and simple.
+            /// TODO: find a solution
+            _kmer_position += 1;
+        }
+    }
+    return { key, _kmer_position, success };
+}
+
+template <>
+kmer_iterator<no_ambiguity_policy>::kmer_iterator(std::string_view sequence_view, size_t kmer_size) noexcept
+    : _sequence_view{ sequence_view }, _kmer_size{ kmer_size }, _kmer_value{ phylo_kmer::nan_key }, _kmer_position{ 0 }
 {
     /// _kmer_size == 0 means npos
     if (_kmer_size == 0)
@@ -20,56 +57,25 @@ kmer_iterator::kmer_iterator(std::string_view sequence_view, size_t kmer_size) n
         bool stop = false;
         while (!stop)
         {
-            const auto [key_and_pos, valid] = encode_from(_kmer_position);
-            std::tie(_current_key, _kmer_position) = key_and_pos;
-            stop = valid || (_kmer_position == std::string_view::npos);
+            bool is_valid = false;
+            std::tie(_kmer_value, _kmer_position, is_valid) = encode_kmer();
+            stop = is_valid || (_kmer_position == std::string_view::npos);
         }
     }
 }
 
-kmer_iterator& kmer_iterator::operator++()
+template<>
+kmer_iterator<no_ambiguity_policy>::value_type kmer_iterator<no_ambiguity_policy>::operator*() const noexcept
 {
-    bool last_kmer_is_valid = true;
-    bool stop = false;
-    while (!stop)
-    {
-        /// if the last k-mer is valid, we can reuse its code in a rolling fashion
-        if (last_kmer_is_valid)
-        {
-            const auto [key_and_pos, valid] = next_kmer();
-            std::tie(_current_key, _kmer_position) = key_and_pos;
-            stop = valid || (_kmer_position == std::string_view::npos);
-            last_kmer_is_valid = valid;
-        }
-        /// otherwise we need to calculate the code from scratch
-        else
-        {
-            const auto [key_and_pos, valid] = encode_from(_kmer_position);
-            std::tie(_current_key, _kmer_position) = key_and_pos;
-            stop = valid || (_kmer_position == std::string_view::npos);
-        }
-    }
-    return *this;
+    return { _sequence_view.substr(_kmer_position, _kmer_size), _kmer_value };
 }
 
-bool kmer_iterator::operator==(const kmer_iterator& rhs) const noexcept
+template<>
+std::tuple<no_ambiguity_policy::value_type,
+    kmer_iterator<no_ambiguity_policy>::pos_t,
+    bool> kmer_iterator<no_ambiguity_policy>::encode_kmer_from_previous() const
 {
-    return (_kmer_position == rhs._kmer_position) && (_sequence_view == rhs._sequence_view);
-}
-
-bool kmer_iterator::operator!=(const kmer_iterator& rhs) const noexcept
-{
-    return !(*this == rhs);
-}
-
-kmer_iterator::value_type kmer_iterator::operator*() const noexcept
-{
-    return { _sequence_view.substr(_kmer_position, _kmer_size), _current_key };
-}
-
-std::pair<std::pair<phylo_kmer::key_type, size_t>, bool> kmer_iterator::next_kmer() const
-{
-    auto key = _current_key;
+    auto key = _kmer_value;
     auto position = _kmer_position;
     bool success = false;
 
@@ -90,53 +96,20 @@ std::pair<std::pair<phylo_kmer::key_type, size_t>, bool> kmer_iterator::next_kme
             key |= *new_code;
             success = true;
         }
-        /// if we found a gap or ambiguity, skip it and go to the next possible k-mer
+            /// if we found a gap or ambiguity, skip it and go to the next possible k-mer
         else
         {
             position = position + _kmer_size;
         }
     }
-    /// if we reached the end of sequence
+        /// if we reached the end of sequence
     else
     {
         position = std::string_view::npos;
     }
-    return { { key, position }, success };
+    return { key, position, success };
 }
 
-std::pair<std::pair<phylo_kmer::key_type, size_t>, bool> kmer_iterator::encode_from(size_t position) const
-{
-    phylo_kmer::key_type key = phylo_kmer::nan_key;
-    bool success = false;
-
-    /// string is too small to start from _kmer_position
-    if (position > _sequence_view.size() - _kmer_size)
-    {
-        position = std::string_view::npos;
-    }
-    else
-    {
-        const auto kmer_view = _sequence_view.substr(_kmer_position, _kmer_size);
-
-        /// correct k-mer with gaps
-        if (auto code = core::encode_kmer(kmer_view); code)
-        {
-            key = *code;
-            success = true;
-        }
-        /// skip a k-mer if we found a gap or ambiguity
-        else
-        {
-            /// WARNING:
-            /// this is not effective, because we can skip all the bases up to the one
-            /// that is not valid, not just only one. But this is not clear how to return
-            /// this position from core::encode_kmer, still having the API of the core clear and simple.
-            /// TODO: find a solution
-            position += 1;
-        }
-    }
-    return { { key, position }, success };
-}
 
 to_kmers::to_kmers(std::string_view sequence_view, size_t kmer_size) noexcept
     : _sequence_view{ sequence_view }, _kmer_size{ kmer_size }
