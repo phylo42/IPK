92ead7e3dca7a68b3556031a000c5c0260aef55e
Nikolai Romashchenko <nikolay.romashchenko@gmail.com>
2020-05-04 12:44:40 +0200

    Updated xpas: core -> xpas

--- pyxpas/src/pyxpas.cpp
+++ pyxpas/src/pyxpas.cpp
@@ -1,10 +1,10 @@
 #include <boost/python.hpp>
 #include <boost/python/numpy.hpp>
 #include <boost/python/numpy/dtype.hpp>
-#include <core/phylo_kmer_db.h>
-#include <core/serialization.h>
+#include <xpas/phylo_kmer_db.h>
+#include <xpas/serialization.h>
 
-using namespace core;
+using namespace xpas;
 namespace bp = boost::python;
 namespace bn = boost::python::numpy;
 
@@ -38,7 +38,7 @@ void translate(const std::exception& e)
 /// Convert std::optional<search_result> to a python object
 struct to_python_search_result
 {
-    static PyObject* convert(const std::optional<core::impl::search_result>& obj)
+    static PyObject* convert(const std::optional<xpas::impl::search_result>& obj)
     {
         if (obj)
         {
@@ -49,7 +49,7 @@ struct to_python_search_result
             /// We can throw an exception if nothing found, but it is better to
             /// create an empty vector of entries to pass to python side as a result.
             phylo_kmer_db::value_type empty_collection;
-            core::impl::search_result empty_result(empty_collection.begin(), empty_collection.end());
+            xpas::impl::search_result empty_result(empty_collection.begin(), empty_collection.end());
 
             return bp::incref(bp::object(empty_result).ptr());
         }
@@ -58,18 +58,18 @@ struct to_python_search_result
 
 struct db_entry
 {
-    db_entry(core::phylo_kmer_db::key_type key, std::vector<core::pkdb_value> values)
+    db_entry(xpas::phylo_kmer_db::key_type key, std::vector<xpas::pkdb_value> values)
         : _key {key}, _values{ std::move(values) } {}
 
-    core::phylo_kmer_db::key_type _key;
-    std::vector<core::pkdb_value> _values;
+    xpas::phylo_kmer_db::key_type _key;
+    std::vector<xpas::pkdb_value> _values;
 };
 
 bp::tuple db_entry_iter(db_entry& entry)
 {
     Py_intptr_t shape[1] = { static_cast<Py_intptr_t>(entry._values.size()) };
 
-    const auto npuint = bn::dtype::get_builtin<core::phylo_kmer::branch_type>();
+    const auto npuint = bn::dtype::get_builtin<xpas::phylo_kmer::branch_type>();
     auto branches = bn::zeros(1, shape, npuint);
 
     const auto npfloat = bn::dtype::get_builtin<float>();
@@ -98,10 +98,10 @@ bn::ndarray db_as_matrix(const phylo_kmer_db& db)
     }
 
     /// find the number of k-mers
-    const size_t max_key = std::pow(core::seq_traits::alphabet_size, db.kmer_size());
+    const size_t max_key = std::pow(xpas::seq_traits::alphabet_size, db.kmer_size());
 
     const auto shape = bp::make_tuple(max_key, num_branches);
-    const auto np_score_type = bn::dtype::get_builtin<core::phylo_kmer::score_type>();
+    const auto np_score_type = bn::dtype::get_builtin<xpas::phylo_kmer::score_type>();
     auto matrix = bn::zeros(shape, np_score_type);
 
     for (const auto& [key, entries] : db)
@@ -146,7 +146,7 @@ BOOST_PYTHON_MODULE(pyxpas)
     /// Python wrapping of std::optional.
     /// This is an adaption of the solution from stackoverlow.
     /// See https://stackoverflow.com/questions/36485840/wrap-boostoptional-using-boostpython
-    bp::to_python_converter<std::optional<core::impl::search_result>, to_python_search_result>();
+    bp::to_python_converter<std::optional<xpas::impl::search_result>, to_python_search_result>();
 
     /// a pair { branch -> score }
     bp::class_<pkdb_value>("PkDbValue", bp::init<phylo_kmer::branch_type, phylo_kmer::score_type>())
@@ -155,21 +155,21 @@ BOOST_PYTHON_MODULE(pyxpas)
     ;
 
     /// a search result: collection of pkdb_value
-    bp::class_<core::impl::search_result>("PkDbSearchResult")
-        .def("__iter__", bp::range(&core::impl::search_result::begin, &core::impl::search_result::end))
+    bp::class_<xpas::impl::search_result>("PkDbSearchResult")
+        .def("__iter__", bp::range(&xpas::impl::search_result::begin, &xpas::impl::search_result::end))
     ;
 
     /*bp::class_<phylo_kmer_db::storage::const_iterator::value_type>( "PkDbEntry", )
         //.def("key", &phylo_kmer_db::storage::const_iterator::value_type::first)
         //.def("value", &phylo_kmer_db::storage::const_iterator::value_type::second)
     ;*/
-    bp::class_<db_entry>("PkDbEntry", bp::init<core::phylo_kmer_db::key_type, std::vector<core::pkdb_value>>())
+    bp::class_<db_entry>("PkDbEntry", bp::init<xpas::phylo_kmer_db::key_type, std::vector<xpas::pkdb_value>>())
         .def("as_row", &db_entry_iter)
     ;
     bp::to_python_converter<phylo_kmer_db::storage::const_iterator::value_type, to_python_db_entry>();
 
     /// phylo k-mer database class
-    bp::class_<phylo_kmer_db, boost::noncopyable, std:: shared_ptr<phylo_kmer_db>>("PhyloKmerDb", bp::init<size_t, core::phylo_kmer::score_type, std::string>())
+    bp::class_<phylo_kmer_db, boost::noncopyable, std:: shared_ptr<phylo_kmer_db>>("PhyloKmerDb", bp::init<size_t, xpas::phylo_kmer::score_type, std::string>())
         .def("search", &phylo_kmer_db::search)
         .def("size", &phylo_kmer_db::size)
         .def("kmer_size", &phylo_kmer_db::kmer_size)
@@ -187,6 +187,6 @@ BOOST_PYTHON_MODULE(pyxpas)
             /// core::phylo_kmer_db is move-able only. core::load returns an object, relying on
             /// guaranteed copy elision. But this mechanism does not exist in python.
             /// To avoid making phylo_kmer_db copyable, here we use smart pointers instead.
-            return std::make_shared<phylo_kmer_db>(core::load(filename));
+            return std::make_shared<phylo_kmer_db>(xpas::load(filename));
         });
 }
