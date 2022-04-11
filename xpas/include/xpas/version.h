b9be4797b2122cd7269644f651f4cd20d51934e8
Nikolai Romashchenko <nikolay.romashchenko@gmail.com>
2022-04-11 12:13:59 +0200

    v0.3.1

--- xpas/include/xpas/version.h
+++ xpas/include/xpas/version.h
@@ -9,8 +9,8 @@ namespace xpas
     struct version
     {
         static constexpr auto major = "0";
-        static constexpr auto minor = "2";
-        static constexpr auto revision = "0";
+        static constexpr auto minor = "3";
+        static constexpr auto revision = "1";
 
         /// \brief Returns the core version number as a string
         static std::string as_string()
