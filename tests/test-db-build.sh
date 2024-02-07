e003ec37ee3ceee38706173c91825877a1e152fd
Nikolai Romashchenko <nikolay.romashchenko@gmail.com>
2024-02-07 18:12:51 +0100

    [CI]

--- tests/test-db-build.sh
+++ tests/test-db-build.sh
@@ -35,11 +35,11 @@ DATABASE_BUILD="${WORKING_DIR}"/DB_k7_o2.0.rps
 
 if [ ! -f "${IPK_BIN}" ]
 then
-    echo "Error: could not find binary files of IPK: ${IPK_BIN}. Please make sure to compile the project"
+    echo "Error: could not find binary files of IPK: ${IPK_BIN}. Please make sure to compile the project" >> $GITHUB_OUTPUT
     exit 1
 elif [ ! -f "${IPK_DIFF_BIN}" ]
 then
-    echo "Error: could not find tools: ${IPK_DIFF_BIN}. Please make sure to compile it separately, i.e. do 'make diff-dna' or 'cmake --build DIR --target diff-dna"
+    echo "Error: could not find tools: ${IPK_DIFF_BIN}. Please make sure to compile it separately, i.e. do 'make diff-dna' or 'cmake --build DIR --target diff-dna" >> $GITHUB_OUTPUT
     exit 2
 elif [ ! "${RAXML_NG}" ]
 then
@@ -52,21 +52,21 @@ else
 
     command=python3 "${IPK_SCRIPT}" build -r "${REFERENCE}" -t "${TREE}" -m GTR -k 7 --omega 2.0 -u 1.0 -b "${RAXML_NG}" -w "${WORKING_DIR}"
 
-    echo "Binary files: OK. Running IPK as: ${command}"
-    eval "${command}"
+    echo "Binary files: OK. Running IPK as: ${command}" >> $GITHUB_OUTPUT
+    eval "${command}" 
 
     if [ ! -f "${DATABASE_BUILD}" ]
     then
-        echo "Error: could not find ${DATABASE_BUILD}. Something went wrong"
+        echo "Error: could not find ${DATABASE_BUILD}. Something went wrong" >> $GITHUB_OUTPUT
         exit 4
     fi
 
     $IPK_DIFF_BIN 0 "${DATABASE_REFERENCE}" "${DATABASE_BUILD}"
 
     if [ $? -ne 0 ]; then
-        echo "Error: databases are different. See the ipkdiff log"
+        echo "Error: databases are different. See the ipkdiff log" >> $GITHUB_OUTPUT
         exit 5
     else
-        echo "OK! Databases are the same."
+        echo "OK! Databases are the same." >> $GITHUB_OUTPUT
     fi
 fi
